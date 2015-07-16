5a76f6db0c8df155e4fdd48e0fd45b54d36e0611
Michael Shattuck <ms2474@gmail.com>
2015-07-16 15:18:33 -0600

    Add IHasher implementation.

---
+++ FeatureToggle/Hasher.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FeatureToggle
+{
+    public class Hasher : IHasher
+    {
+        public byte[] Hash(string input)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Encoding.UTF8.GetBytes(input ?? ""));
+            }
+        }
+    }
+}
---
+++ FeatureToggleSpecs/HasherSpecs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FeatureToggle;
+using NUnit.Framework;
+
+namespace FeatureToggleSpecs
+{
+    [TestFixture]
+    public class HasherSpecs
+    {
+        Hasher ClassUnderTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ClassUnderTest = new Hasher();
+        }
+
+        [Test]
+        public void When_hashing_a_string()
+        {
+            var result = ClassUnderTest.Hash("this-is-a-string");
+
+            Assert.That(result.Length, Is.EqualTo(32));
+            Assert.That(Convert.ToBase64String(result), Is.EqualTo("WZfVC42xIJ8ZNIdUvqjGt+GqTGE9/Nm4qF6c97EMTE4="));
+        }
+
+        [Test]
+        public void When_hashing_an_empty_string()
+        {
+            var result = ClassUnderTest.Hash("");
+
+            Assert.That(result.Length, Is.EqualTo(32));
+            Assert.That(Convert.ToBase64String(result), Is.EqualTo("47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="));
+        }
+
+        [Test]
+        public void When_hashing_a_null_value()
+        {
+            var result = ClassUnderTest.Hash(null);
+
+            Assert.That(result.Length, Is.EqualTo(32));
+            Assert.That(Convert.ToBase64String(result), Is.EqualTo("47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU="));
+        }
+    }
+}
